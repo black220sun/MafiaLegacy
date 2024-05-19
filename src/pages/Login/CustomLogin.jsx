ef3e573eac8222b9f3e2ed151ade063cef0b22ce
Vladislav Shulepov <shulepov.vlad@gmail.com>
2024-05-19 14:50:30 +0200

    Rename the app

--- src/pages/Login/CustomLogin.jsx
+++ src/pages/Login/CustomLogin.jsx
@@ -10,10 +10,9 @@ const CustomLogin = () => {
 		<div className={style.login}>
 			<video src="https://i.imgur.com/50L3hwC.mp4" className={style.video} autoPlay muted loop></video> {/* Видеофон */}
 			<div className={style.glass}></div> {/* Затемнение заднего фона */}
-			<h1 className={style.h1}>Mafia's Legacy</h1> {/* Заголовок с названием игры */}
+			<h1 className={style.h1}>MEOWFIA</h1> {/* Заголовок с названием игры */}
 			<div className={style.infoAuth}>
-				<h2 className={style.h2}>Developed by Liunamme</h2> {/* Заголовок с автором приложения */}
-				<h3 className={style.h3}>Нужен доступ к приложению? Пиши в DISCORD: <a href="https://discordapp.com/users/312291477864841216/" target='_blank'>@liunamme</a></h3> {/* Заголовок с информацией по доступу к приложению */}
+				<h2 className={style.h2}>Originally developed by Liunamme</h2> {/* Заголовок с автором приложения */}
 			</div>
 			<Login /> {/* Компонент авторизации (поле пароль и кнопка войти) */}
 		</div>
